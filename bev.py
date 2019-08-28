6fbb60258ab74c5e33c02d791c31bad2d0f5e5bb
Manny Sayah <31831425+mannysayah@users.noreply.github.com>
2019-08-28 17:02:43 +1200

    Add files via upload

---
+++ bev.py
@@ -0,0 +1,11 @@
+import bpy
+
+outputFile = '{where you want to save it}/mesh.csv'
+
+verts = [ bpy.context.object.matrix_world @ v.co for v in bpy.context.object.data.vertices ]
+
+rows = [ ";".join([ str(v) for v in co ]) + "\n" for co in verts ]
+
+f = open( outputFile, 'w' )
+f.writelines( rows )
+f.close()
